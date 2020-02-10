48fbc21dc30a90593b70fd38f95689078748753b
John Hayduk <jhayduk@cisco.com>
2020-02-09 19:05:36 -0500

    Update to run Step 1 of the Pixel tutorial to draw a blank skyblue window.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # go-training
 Simple Repo to Do Some Go Training In
+
+For the moment, this is following the Tutorial at 
+https://github.com/faiface/pixel/wiki for using the Pixel Game Engine:
+https://github.com/faiface/pixel 
--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+	"golang.org/x/image/colornames"
+)
 
+// run is the main function of the game. It performs the initialization
+// of the main game window and contains the update loop to continuously
+// redraw the window.
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Pixel Rocks!",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		// Limit screen updates to the refresh rate of the monitor.
+		VSync: true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	// Clear the game screen by painting the whole screen Skyblue.
+	win.Clear(colornames.Skyblue)
+
+	for !win.Closed() {
+		win.Update()
+	}
+
+}
+
+//
+// The main function only calls pxelgl.Run(run) to let the run() function,
+// which is the main function for the game, execute in the main thread.
 func main() {
-	fmt.Println("hello")
+	pixelgl.Run(run)
 }
