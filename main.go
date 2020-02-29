bc1ef1055884ddc683d209762391bf1c4c2d04eb
John Hayduk <jhayduk@cisco.com>
2020-02-29 15:27:59 -0500

    After doing https://github.com/faiface/pixel/wiki/Drawing-a-Sprite

--- main.go
+++ main.go
@@ -1,14 +1,39 @@
 package main
 
 import (
+	"image"
+	"os"
+
+	// Import the image file formats that are to be supported
+	_ "image/png"
+
 	"github.com/faiface/pixel"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
 
+//
+// loadPicture is a helper function to load pictures from files
+// into a PictureData object.
+//
+func loadPicture(path string) (pixel.Picture, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	img, _, err := image.Decode(file)
+	if err != nil {
+		return nil, err
+	}
+	return pixel.PictureDataFromImage(img), nil
+}
+
+//
 // run is the main function of the game. It performs the initialization
 // of the main game window and contains the update loop to continuously
 // redraw the window.
+//
 func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
@@ -21,9 +46,31 @@ func run() {
 		panic(err)
 	}
 
-	// Clear the game screen by painting the whole screen Skyblue.
-	win.Clear(colornames.Skyblue)
+	//
+	// Load the "hiking gopher" picture and create a sprite from it.
+	// For now, this is the player character (I presume)
+	//
+	pic, err := loadPicture("hiking.png")
+	if err != nil {
+		panic(err)
+	}
+	sprite := pixel.NewSprite(pic, pic.Bounds())
 
+	win.Clear(colornames.Greenyellow)
+	//
+	// Note that sprites are anchored by their centers, and the x,y axis
+	// starting at the lower left and increasing "naturally" (x increases
+	// to the right and y increases upward).
+	//
+	// Moved(win.Bounds().Center()) does not, technically, mean "move to the
+	// center of the window", it means "move by the amount specified by a
+	// vector starting at the origin and ending at the center of the window".
+	//
+	sprite.Draw(win, pixel.IM.Moved(win.Bounds().Center()))
+
+	//
+	// Continuously update the window until the close button is hit
+	//
 	for !win.Closed() {
 		win.Update()
 	}
